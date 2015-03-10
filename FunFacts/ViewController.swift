03455261f52d24300aabd8893075556ad6fb6ba4
matmenzl <mathias.menzl@gmail.com>
2015-03-10 16:08:18 +0100

    first commit

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var parser = NSXMLParser()
+    var feed = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var ftitle = NSMutableString()
+    var link = NSMutableString()
+    var fdescription = NSMutableString()
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+        
+    }
 
 }
 
---
+++ FunFacts/scratchPad.playground/section-1.swift
@@ -0,0 +1,33 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+
+// var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+// Optional
+
+
+// Random Number Generator
+
+var randomNumber = Int (arc4random_uniform(10))
+
+
+// UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+let colorsArray
+
+
+
